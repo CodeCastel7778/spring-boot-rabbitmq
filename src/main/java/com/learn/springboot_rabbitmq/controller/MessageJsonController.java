66456792ce74a00d38bf87d2e37d0c027bc99f79
Jayanth Kumar <j.ei7778@gmail.com>
2024-11-17 20:38:21 +0530

    Created Rest API to JSON object to RabbitMQ producer

---
+++ src/main/java/com/learn/springboot_rabbitmq/controller/MessageJsonController.java
@@ -0,0 +1,24 @@
+package com.learn.springboot_rabbitmq.controller;
+
+import com.learn.springboot_rabbitmq.dto.User;
+import com.learn.springboot_rabbitmq.producer.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private final RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user) {
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("JSON message sent to RabbitMQ.");
+    }
+
+}
